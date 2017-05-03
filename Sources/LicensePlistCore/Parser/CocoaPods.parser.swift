868d7035a53b2ab250e946017942fb3438192ac2
Masayuki Ono <mono0926@gmail.com>
2017-05-03 19:13:17 +0900

    Refactor

--- Sources/LicensePlistCore/Parser/CocoaPods.parser.swift
+++ Sources/LicensePlistCore/Parser/CocoaPods.parser.swift
@@ -5,33 +5,14 @@ extension CocoaPodsLicense: Parser {
     public static func parse(_ content: String) -> [CocoaPodsLicense] {
         return SWXMLHash.config { _ in }.parse(content)["plist"]["dict"]["array"].children
             .map { e in
-                var title: String?
-                var license: String?
-                var titleIndex: Int?
-                var licenseIndex: Int?
-                var isLicense = false
-                e.children.map { $0.element?.text }.flatMap { $0 }.enumerated().forEach { (i, text) in
-                    if text == "License" {
-                        isLicense = true
-                    }
-                    if text == "Title" {
-                        titleIndex = i + 1
-                        return
-                    }
-                    if text == "FooterText" {
-                        licenseIndex = i + 1
-                        return
-                    }
-                    if i == titleIndex {
-                        title = text
-                        return
-                    }
-                    if i == licenseIndex {
-                        license = text
-                        return
-                    }
+                let elements = e.children.map { $0.element?.text }.flatMap { $0 }
+                let dict = zip(elements, elements.dropFirst())
+                    .reduce([String: String]()) { sum, e in
+                        var sum = sum
+                        sum[e.0] = e.1
+                        return sum
                 }
-                if let title = title, let license = license, isLicense {
+                if let title = dict["Title"], let license = dict["FooterText"], let _ = dict["License"] {
                     return CocoaPodsLicense(library: CocoaPods(name: title), body: license)
                 }
                 return nil
