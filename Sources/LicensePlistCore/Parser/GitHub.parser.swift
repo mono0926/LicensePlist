74f5d34d765b11fe301dae1be14c36e5497fb58e
Masayuki Ono <mono0926@gmail.com>
2017-05-04 15:28:25 +0900

    Support config file
    - Close #12, #10

--- Sources/LicensePlistCore/Parser/GitHub.parser.swift
+++ Sources/LicensePlistCore/Parser/GitHub.parser.swift
@@ -2,8 +2,11 @@ import Foundation
 
 extension GitHub: Parser {
     public static func parse(_ content: String) -> [GitHub] {
+        return parse(content, mark: "github ")
+    }
+    public static func parse(_ content: String, mark: String, quotes: String = "\"") -> [GitHub] {
         let pattern = "[\\w\\.\\-]+"
-        let regex = try! NSRegularExpression(pattern: "github \"(\(pattern))/(\(pattern))\"", options: [])
+        let regex = try! NSRegularExpression(pattern: "\(mark)\(quotes)(\(pattern))/(\(pattern))\(quotes)", options: [])
         let nsContent = content as NSString
         let matches = regex.matches(in: content, options: [], range: NSRange(location: 0, length: nsContent.length))
         return matches.map { match -> GitHub? in
---
+++ Tests/LicensePlistTests/ConfigLoaderTests.swift
@@ -0,0 +1,16 @@
+import Foundation
+import XCTest
+@testable import LicensePlistCore
+
+class ConfigLoaderTests: XCTestCase {
+    private let target = ConfigLoader.shared
+    func testLoad_empty() {
+        XCTAssertEqual(target.load(yaml: ""), Config(githubs: [], excludes: []))
+    }
+    func testLoad_sample() {
+        let path = "https://raw.githubusercontent.com/mono0926/LicensePlist/master/Tests/LicensePlistTests/Resources/license_plist.yml"
+        XCTAssertEqual(target.load(yaml: URL(string: path)!.downloadContent().blocking().result!.value!),
+                       Config(githubs: [GitHub(name: "NativePopup", owner: "mono0926")],
+                              excludes: ["RxSwift", "ios-license-generator"]))
+    }
+}
