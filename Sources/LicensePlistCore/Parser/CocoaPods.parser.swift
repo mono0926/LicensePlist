e645b6d7bd721494244d404ffdf8d215423bfcb4
Masayuki Ono <mono0926@gmail.com>
2017-05-07 18:42:29 +0900

    Refactor parsing plist

--- Sources/LicensePlistCore/Parser/CocoaPods.parser.swift
+++ Sources/LicensePlistCore/Parser/CocoaPods.parser.swift
@@ -1,22 +1,48 @@
 import Foundation
 import SWXMLHash
+import Himotoki
+import LoggerAPI
 
 extension CocoaPodsLicense: Parser {
     public static func parse(_ content: String) -> [CocoaPodsLicense] {
-        return SWXMLHash.config { _ in }.parse(content)["plist"]["dict"]["array"].children
-            .map { e in
-                let elements = e.children.map { $0.element?.text }.flatMap { $0 }
-                let dict = zip(elements, elements.dropFirst())
-                    .reduce([String: String]()) { sum, e in
-                        var sum = sum
-                        sum[e.0] = e.1
-                        return sum
-                }
-                if let title = dict["Title"], let license = dict["FooterText"], dict["License"] != nil {
-                    return CocoaPodsLicense(library: CocoaPods(name: title), body: license)
-                }
-                return nil
-            }
-            .flatMap { $0 }
+        do {
+            let plist = try PropertyListSerialization.propertyList(from: content.data(using: String.Encoding.utf8)!,
+                                                                    options: [],
+                                                                    format: nil)
+
+            return try AcknowledgementsPlist.decodeValue(plist).preferenceSpecifiers
+                .filter { $0.isLicense }
+                .map { CocoaPodsLicense(library: CocoaPods(name: $0.title), body: $0.footerText) }
+        } catch let e {
+            Log.error(String(describing: e))
+            return []
+        }
+    }
+}
+
+struct AcknowledgementsPlist {
+    let preferenceSpecifiers: [PreferenceSpecifier]
+}
+
+extension AcknowledgementsPlist: Decodable {
+    static func decode(_ e: Extractor) throws -> AcknowledgementsPlist {
+        return try AcknowledgementsPlist(preferenceSpecifiers: e.array("PreferenceSpecifiers"))
+    }
+}
+
+struct PreferenceSpecifier {
+    let footerText: String
+    let title: String
+    let type: String
+    let license: String?
+    var isLicense: Bool { return license != nil }
+}
+
+extension PreferenceSpecifier: Decodable {
+    static func decode(_ e: Extractor) throws -> PreferenceSpecifier {
+        return try PreferenceSpecifier(footerText: e.value("FooterText"),
+                                       title: e.value("Title"),
+                                       type: e.value("Type"),
+                                       license: e.valueOptional("License"))
     }
 }
