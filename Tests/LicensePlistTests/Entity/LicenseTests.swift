9d8e710ba3ef803acecca5e05ce8cd67d0dac50c
Masayuki Ono <mono0926@gmail.com>
2017-05-11 20:33:57 +0900

    Suppress double escape

--- Tests/LicensePlistTests/Entity/LicenseTests.swift
+++ Tests/LicensePlistTests/Entity/LicenseTests.swift
@@ -7,4 +7,8 @@ class LicenseTests: XCTestCase {
         let target = CocoaPodsLicense(library: CocoaPods(name: "", version: nil), body: "body&\"'><")
         XCTAssertEqual(target.bodyEscaped, "body&amp;&quot;&#x27;&gt;&lt;")
     }
+    func testBodyEscaped_escaped() {
+        let target = CocoaPodsLicense(library: CocoaPods(name: "", version: nil), body: "body&amp;")
+        XCTAssertEqual(target.bodyEscaped, "body&amp;")
+    }
 }
