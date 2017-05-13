8e3fc3d3a78958c543e3407a22101326f5151794
Masayuki Ono <mono0926@gmail.com>
2017-05-13 10:32:19 +0900

    Enhance extension

--- Sources/LicensePlistCore/Extension/URL.extension.swift
+++ Sources/LicensePlistCore/Extension/URL.extension.swift
@@ -1,11 +1,13 @@
 import Foundation
 import Result
 
-public extension URL {
-    public func downloadContent() -> ResultOperation<String, NSError> {
+extension URL: ExtensionCompatible {}
+
+extension Extension where Base == URL {
+    func downloadContent() -> ResultOperation<String, NSError> {
         let operation =  ResultOperation<String, NSError> { _ in
             do {
-                return Result(value: try String(contentsOf: self))
+                return Result(value: try String(contentsOf: self.base))
             } catch let e {
                 return Result(error: e as NSError)
             }
