9653753e695d11480df7f8c7eafbc8309de3b2d8
Masayuki Ono <mono0926@gmail.com>
2017-05-15 15:52:42 +0900

    Enhance install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,10 @@ curl "https://api.github.com/repos/mono0926/LicensePlist/releases" \
      | jq -r '.[0].browser_download_url' \
      | xargs -n1 curl -LOk
 unzip license-plist.zip
+if [ $? -ne 0 ]
+then
+    echo "Filed to download the latest executable binary. Maybe the GitHub API limit occurs. Try other installation methods(see: https://github.com/mono0926/LicensePlist#installation )."
+    exit 1
+fi
 cp -f license-plist /usr/local/bin/license-plist
 echo "Installed at $(which license-plist) 🎉"
