4369be0412ecd13abb1ce0ecca429606b95cc1b1
Masayuki Ono <mono0926@gmail.com>
2017-05-17 13:44:29 +0900

    Fix license-plist

--- Tests/LicensePlistTests/Entity/PlistInfoTests.swift
+++ Tests/LicensePlistTests/Entity/PlistInfoTests.swift
@@ -76,7 +76,7 @@ class PlistInfoTests: XCTestCase {
         target.compareWithLatestSummary()
 
         XCTAssertEqual(target.summary,
-                       "add-version-numbers: false\n\nLicensePlist Version: 1.3.8")
+                       "add-version-numbers: false\n\nLicensePlist Version: 1.4.0")
         XCTAssertNotNil(target.summaryPath)
     }
 
