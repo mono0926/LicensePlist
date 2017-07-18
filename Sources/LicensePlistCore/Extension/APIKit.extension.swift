4f0bd2ef19ecdd5bb968a883a8bf473a2722c716
JPMartha <jpmartha.jp@gmail.com>
2017-07-18 17:00:09 +0900

    Fix typo in LicensePlistExtension

--- Sources/LicensePlistCore/Extension/APIKit.extension.swift
+++ Sources/LicensePlistCore/Extension/APIKit.extension.swift
@@ -7,12 +7,12 @@ extension Session: LicensePlistCompatible {}
 extension LicensePlistExtension where Base: Session {
     func sendSync<T: Request>(_ request: T) -> Result<T.Response, SessionTaskError> {
         var result: Result<T.Response, SessionTaskError>!
-        let semaphor = DispatchSemaphore(value: 0)
+        let semaphore = DispatchSemaphore(value: 0)
         self.base.send(request, callbackQueue: .sessionQueue) { _result in
             result = _result
-            semaphor.signal()
+            semaphore.signal()
         }
-        semaphor.wait()
+        semaphore.wait()
         return result
     }
     func send<T: Request>(_ request: T) -> ResultOperation<T.Response, SessionTaskError> {
