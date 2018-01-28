29476663a48a0d42f9fbcde3fbc80d3c52842cd2
Masayuki Ono <mono0926@gmail.com>
2018-01-28 18:12:50 +0900

    Bump version

--- Sources/LicensePlistCore/Consts.swift
+++ Sources/LicensePlistCore/Consts.swift
@@ -6,6 +6,6 @@ public struct Consts {
     public static let prefix = "com.mono0926.LicensePlist"
     public static let outputPath = "\(prefix).Output"
     public static let configPath = "license_plist.yml"
-    public static let version = "1.8.2"
+    public static let version = "1.8.3"
     public static let encoding = String.Encoding.utf8
 }
--- Tests/LicensePlistTests/Entity/PlistInfoTests.swift
+++ Tests/LicensePlistTests/Entity/PlistInfoTests.swift
@@ -78,7 +78,7 @@ class PlistInfoTests: XCTestCase {
         target.compareWithLatestSummary()
 
         XCTAssertEqual(target.summary,
-                       "add-version-numbers: false\n\nLicensePlist Version: 1.8.2")
+                       "add-version-numbers: false\n\nLicensePlist Version: 1.8.3")
         XCTAssertNotNil(target.summaryPath)
     }
 
