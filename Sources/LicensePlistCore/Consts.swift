a1a0bcdbaa9e197863b5264159a15d649d43ff9f
Masayuki Ono <mono0926@gmail.com>
2019-10-03 17:58:24 +0900

    Bump up version

--- Sources/LicensePlistCore/Consts.swift
+++ Sources/LicensePlistCore/Consts.swift
@@ -7,6 +7,6 @@ public struct Consts {
     public static let prefix = "com.mono0926.LicensePlist"
     public static let outputPath = "\(prefix).Output"
     public static let configPath = "license_plist.yml"
-    public static let version = "2.7.2"
+    public static let version = "2.7.3"
     public static let encoding = String.Encoding.utf8
 }
