de26112bb821fe63f0f710f482f13a231abef6b8
funzin <nakazawa.fumito@gmail.com>
2020-04-05 18:41:56 +0900

    Add GitHubLibraryConfigFile

---
+++ Tests/LicensePlistTests/Entity/GitHubLibraryConfigFileTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import LicensePlistCore
+
+class GitHubLibraryConfigFileTests: XCTestCase {
+
+    func testInit() {
+        XCTAssertEqual(GitHubLibraryConfigFile.carthage(content: "content"), GitHubLibraryConfigFile(type: .carthage, content: "content"))
+        XCTAssertEqual(GitHubLibraryConfigFile.mint(content: "content"), GitHubLibraryConfigFile(type: .mint, content: "content"))
+        XCTAssertEqual(GitHubLibraryConfigFile.licensePlist(content: "content"), GitHubLibraryConfigFile(type: .licensePlist, content: "content"))
+    }
+}
