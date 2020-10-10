f41a03840ccdc3e52b6a1d5e6af8e9b8ec688a2d
Benedek Kozma <cyberbeni@gmail.com>
2020-10-11 00:48:01 +0200

    add comment about typename

--- Sources/LicensePlist/main.swift
+++ Sources/LicensePlist/main.swift
@@ -1,6 +1,6 @@
+import ArgumentParser
 import Foundation
 import LicensePlistCore
-import ArgumentParser
 import LoggerAPI
 
 private func loadConfig(configPath: URL) -> Config {
@@ -16,6 +16,7 @@ extension CompletionKind {
 	}
 }
 
+// Typename used for usage in help command
 struct LicensePlist: ParsableCommand {
 	@Option(name: .long, completion: .file())
 	var cartfilePath = Consts.cartfileName
@@ -90,44 +91,3 @@ struct LicensePlist: ParsableCommand {
 }
 
 LicensePlist.main()
-
-//let main = command(Option("cartfile-path", default: Consts.cartfileName),
-//                   Option("mintfile-path", default: Consts.mintfileName),
-//                   Option("pods-path", default: Consts.podsDirectoryName),
-//                   Option("package-path", default: Consts.packageName),
-//                   Option("xcodeproj-path", default: "*.xcodeproj"),
-//                   Option("output-path", default: Consts.outputPath),
-//                   Option("github-token", default: ""),
-//                   Option("config-path", default: Consts.configPath),
-//                   Option("prefix", default: Consts.prefix),
-//                   Option("html-path", default: ""),
-//                   Option("markdown-path", default: ""),
-//                   Flag("force"),
-//                   Flag("add-version-numbers"),
-//                   Flag("suppress-opening-directory"),
-//                   Flag("single-page"),
-//                   Flag("fail-if-missing-license")) { cartfile, mintfile, podsPath, packagePath, xcodeprojPath, output, gitHubToken, configPath, prefix, htmlPath, markdownPath, force, version, suppressOpen, singlePage, failIfMissingLicense in
-//
-//                    Logger.configure()
-//                    var config = loadConfig(configPath: URL(fileURLWithPath: configPath))
-//                    config.force = force
-//                    config.addVersionNumbers = version
-//                    config.suppressOpeningDirectory = suppressOpen
-//                    config.singlePage = singlePage
-//                    config.failIfMissingLicense = failIfMissingLicense
-//                    let options = Options(outputPath: URL(fileURLWithPath: output),
-//                                          cartfilePath: URL(fileURLWithPath: cartfile),
-//                                          mintfilePath: URL(fileURLWithPath: mintfile),
-//                                          podsPath: URL(fileURLWithPath: podsPath),
-//                                          packagePath: URL(fileURLWithPath: packagePath),
-//                                          xcodeprojPath: URL(fileURLWithPath: xcodeprojPath),
-//                                          prefix: prefix,
-//                                          gitHubToken: gitHubToken.isEmpty ? ProcessInfo.processInfo.environment["LICENSE_PLIST_GITHUB_TOKEN"] : gitHubToken,
-//                                          htmlPath: htmlPath.isEmpty ? nil : URL(fileURLWithPath: htmlPath),
-//                                          markdownPath: markdownPath.isEmpty ? nil : URL(fileURLWithPath: markdownPath),
-//                                          config: config)
-//                    let tool = LicensePlist()
-//                    tool.process(options: options)
-//}
-//
-//main.run()
