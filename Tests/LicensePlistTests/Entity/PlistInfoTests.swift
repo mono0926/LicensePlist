333c73584579c672c2171d7b4c44115074f06fc5
Masayuki Ono <mono0926@gmail.com>
2021-12-16 16:07:38 +0900

    Bump version

--- Tests/LicensePlistTests/Entity/PlistInfoTests.swift
+++ Tests/LicensePlistTests/Entity/PlistInfoTests.swift
@@ -85,7 +85,7 @@ class PlistInfoTests: XCTestCase {
         target.compareWithLatestSummary()
 
         XCTAssertEqual(target.summary,
-                       "add-version-numbers: false\n\nLicensePlist Version: 3.15.0")
+                       "add-version-numbers: false\n\nLicensePlist Version: 3.16.0")
         XCTAssertNotNil(target.summaryPath)
     }
 
