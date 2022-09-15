443bb6b53fea949b76c5be64010527ddf8de6e53
acevif <acevif@gmail.com>
2022-09-15 16:03:49 +0900

    Refactor

--- Sources/LicensePlistCore/Logger.swift
+++ Sources/LicensePlistCore/Logger.swift
@@ -1,8 +1,9 @@
 import HeliumLogger
 import LoggerAPI
 import Foundation
-import TSCBasic
-import System
+//import TSCBasic
+//import System
+//import Darwin
 
 public struct Logger {
     public static func configure(silenceModeCommandLineFlag: Bool,
@@ -27,7 +28,7 @@ public struct Logger {
             }
         }()
         
-        logger.colored = loggerConfiguration.colored
+        logger.colored = loggerConfiguration.colored.rawValue
         Log.logger = logger
     }
 
@@ -46,7 +47,7 @@ public struct Logger {
 
 fileprivate struct LoggerConfiguration {
     public var silenceMode: Bool
-    public var colored: Bool
+    public var colored: Colored
     public var verbose: Bool
 
     public init(silenceModeCommandLineFlag: Bool,
@@ -56,81 +57,116 @@ fileprivate struct LoggerConfiguration {
         let env: [String:String] = ProcessInfo.processInfo.environment
 
         silenceMode = silenceModeCommandLineFlag
-        colored = Self.color(noColorCommandLineFlag: noColorCommandLineFlag,
+        self.colored = Self.colored(noColorCommandLineFlag: noColorCommandLineFlag,
                         colorCommandLineFlag: colorCommandLineFlag,
                         env: env)
         self.verbose = verboseCommandLineFlag
     }
     
-    private static func color(noColorCommandLineFlag: Bool,
+    enum Colored: RawRepresentable {
+        init?(rawValue: Bool) {
+            switch rawValue {
+            case true: self = .colored
+            case false: self = .noColor
+            }
+        }
+        
+        var rawValue: Bool {
+            switch self {
+            case .colored: return true
+            case .noColor: return false
+            }
+        }
+        
+        typealias RawValue = Bool
+        
+        case colored
+        case noColor
+    }
+    
+    private static func colored(noColorCommandLineFlag: Bool,
                                colorCommandLineFlag: Bool,
-                               env: [String:String]) -> Bool {
+                               env: [String:String]) -> Colored {
         // commandline options:
         if noColorCommandLineFlag {
-            return false
+            return .noColor
         }
         if colorCommandLineFlag {
-            return true
+            return .colored
         }
         
         // environment variable:
         if env[Consts.EnvironmentVariableKey.noColor] == "1" {
-            return false
+            return .noColor
         }
         
         // auto:
-        switch terminalType() {
-        case .file:
-            return false
-        case .dumb:
-            return false // Dumb terminals don't interpret escape sequences
-        case .tty:
-            break // Keep guessing
-        case .none:
-            return false // to be on the safe side
+        func isTTY(_ fd:Int32) -> Bool {
+            return isatty(fd) == 1
         }
-
+        if !isTTY(STDOUT_FILENO) {
+            return .noColor
+        }
+        
         // TODO: detect pipe (and return no-color)
-        // see https://stackoverflow.com/questions/899764/distinguishing-a-pipe-from-a-file-in-unix for more details
+        
+        if env["TERM"] == "dumb" {
+            return .noColor
+        }
         
         if env["TERM"] == "xterm-256color" {
-            return true
+            return .colored
         }
         
-        return false
+        return .noColor // to be on the safe side
     }
 
-    private static func terminalType() -> TerminalController.TerminalType? {
-        // FILEPointer(aka UnsafeMutablePointer<FILE>)はCのFILE*と同じと仮定
-        // fdopenで fileDescriptorから filePointerを作り、
-        // FILEPointerからLocalFileOutputByteStream を作り、terminalTypeを取得
-        let stdOutFileDescriptor: Int32 = {
-            if #available(macOS 11,*) {
-                return FileDescriptor.standardOutput.rawValue as Int32
-            } else {
-                return 1 as Int32
-            }
-        }()
-        // TODO: 本当に標準出力でいいのか確認。標準エラー出力にログを吐いている可能性も
-        
-        do {
-            let mode = "r"
-            let terminalType:TerminalController.TerminalType = try mode.withCString {
-                (modeCString: UnsafePointer<CChar>) -> TerminalController.TerminalType in
-                
-                let filePointer: FILEPointer = fdopen(stdOutFileDescriptor, modeCString)
-                let stream = try LocalFileOutputByteStream(filePointer: filePointer)
-                // TerminalController.terminalType() use isatty() inside.
-                return TerminalController.terminalType(stream)
-            }
-            
-            debugPrint(terminalType) // debug
-            return terminalType
-        } catch is FileSystemError {
-            return nil
-        } catch {
-            assertionFailure("An unexpected/unhandled error occured.")
-            return nil
-        }
-    }
+//    private static func terminalType() -> TerminalController.TerminalType? {
+//        // cf. FILEPointer = UnsafeMutablePointer<FILE> = FILE* in C
+//
+//        // fdopenで fileDescriptorから filePointerを作り、
+//        // FILEPointerからLocalFileOutputByteStream を作り、terminalTypeを取得
+//        let stdOutFileDescriptor: Int32 = {
+////            if #available(macOS 11,*) {
+////                return //FileDescriptor.standardOutput.rawValue as Int32
+////            } else {
+//                return STDOUT_FILENO as Int32
+////            }
+//        }()
+//        // TODO: 本当に標準出力でいいのか確認。標準エラー出力にログを吐いている可能性も
+//
+////        fdopen(<#T##Int32#>, <#T##UnsafePointer<CChar>!#>)
+//
+//        do {
+//            let mode = "r"
+//            let terminalType:TerminalController.TerminalType = try mode.withCString {
+//                (modeCString: UnsafePointer<CChar>) -> TerminalController.TerminalType in
+//
+//                    //open()
+//                let filePointer: FILEPointer = fdopen(stdOutFileDescriptor, O_RDONLY)
+//                let stream = try LocalFileOutputByteStream(filePointer: filePointer)
+//                // TerminalController.terminalType() use isatty() inside.
+//                return TerminalController.terminalType(stream)
+//            }
+//
+//            let isatty = isatty(STDOUT_FILENO)
+//
+//            #if !os(Windows)
+//                    if ProcessInfo.processInfo.environment["TERM"] == "dumb" {
+//                        return .noColor
+//                    }
+//            #endif
+//            let isTTY = isatty(fileno(stream.filePointer)) != 0
+//        return isTTY ? .tty : .file
+//    }
+//
+//            debugPrint(terminalType) // debug
+//            return terminalType
+//        } catch is FileSystemError {
+//            return nil
+//        } catch {
+//            assertionFailure("An unexpected/unhandled error occured.")
+//            return nil
+//        }
+//    }
 }
