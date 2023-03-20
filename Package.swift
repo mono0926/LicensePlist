3179a3aaa54ed8c4ca0d4f55a6db2e21ec95182d
mono <mono0926@gmail.com>
2023-03-20 14:11:15 +0900

    release 3.24.4

--- Package.swift
+++ Package.swift
@@ -87,8 +87,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "LicensePlistBinary",
-            url: "https://github.com/mono0926/LicensePlist/releases/download/3.24.3/LicensePlistBinary-macos.artifactbundle.zip",
-            checksum: "76a79e08d216cf747cb162cb43d976a9f97d8c9a77fabb1eb7e9cb37b9668214"
+            url: "https://github.com/mono0926/LicensePlist/releases/download/3.24.4/LicensePlistBinary-macos.artifactbundle.zip",
+            checksum: "6ff429d07b9015ab476fd0422c262017f227f2938ddc6922cd1f75dd304496a4"
         )
     ]
 )
