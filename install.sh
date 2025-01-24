f9690ffafb830bd5ae0cc755e9f7ea73c9036dea
mono <mono0926@gmail.com>
2025-01-24 14:55:04 +0900

    Fix install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 curl "https://api.github.com/repos/mono0926/LicensePlist/releases" \
-     | jq -r '.[0].assets_url' \
+     | jq -r '.[1].assets_url' \
      | xargs -n1 curl \
-     | jq -r '.[0].browser_download_url' \
+     | jq -r '.[1].browser_download_url' \
      | xargs -n1 curl -LOk
-unzip license-plist.zip
+unzip portable_licenseplist.zip
 if [ $? -ne 0 ]
 then
     echo "Filed to download the latest executable binary. Maybe the GitHub API limit occurs. Try other installation methods(see: https://github.com/mono0926/LicensePlist#installation )."
     exit 1
 fi
-cp -f license-plist /usr/local/bin/license-plist
+sudo cp -f license-plist /usr/local/bin/license-plist
 echo "Installed at $(which license-plist) 🎉"
