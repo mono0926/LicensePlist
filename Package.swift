8c4ffb3c0aef3c3dff4d2880ab68b12aed0f5669
mono <mono0926@gmail.com>
2025-11-20 16:55:51 +0900

    Revert "release 3.27.2"
    This reverts commit 8a83e9e3c788239d57e1a68a89e8c742fb1a460a.

--- Package.swift
+++ Package.swift
@@ -87,8 +87,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "LicensePlistBinary",
-            url: "https://github.com/mono0926/LicensePlist/releases/download/3.27.2/LicensePlistBinary-macos.artifactbundle.zip",
-            checksum: ""
+            url: "https://github.com/mono0926/LicensePlist/releases/download/3.27.1/LicensePlistBinary-macos.artifactbundle.zip",
+            checksum: "24e066b23da58275a89a83cae05bf40cad8a48faacf10facbfa5c350efe02608"
         )
     ]
 )
