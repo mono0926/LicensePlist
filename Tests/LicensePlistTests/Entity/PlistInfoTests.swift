f52df8c68fafd4a8ccdacbb305ec521b9fe22867
mono <mono0926@gmail.com>
2026-04-29 16:34:49 +0900

    Bump

--- Tests/LicensePlistTests/Entity/PlistInfoTests.swift
+++ Tests/LicensePlistTests/Entity/PlistInfoTests.swift
@@ -155,7 +155,7 @@ class PlistInfoTests: XCTestCase {
 
       add-version-numbers: false
 
-      LicensePlist Version: 3.27.6
+      LicensePlist Version: 3.27.7
       """
 
     XCTAssertNil(target.licenses)
