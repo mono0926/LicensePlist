4688a3170ff42c89c0b020ba4e2313de73fb3d4f
mono <mono0926@gmail.com>
2026-09-09 11:28:39 +0900

    3.28.1

--- Tests/LicensePlistTests/Entity/PlistInfoTests.swift
+++ Tests/LicensePlistTests/Entity/PlistInfoTests.swift
@@ -156,7 +156,7 @@ class PlistInfoTests: XCTestCase {
 
       add-version-numbers: false
 
-      LicensePlist Version: 3.28.0
+      LicensePlist Version: 3.28.1
       """
 
     XCTAssertNil(target.licenses)
